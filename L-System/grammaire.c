6c2e65334437903e67578647de86a65e788c8671
Romaric Mangin <romaric.mangin0947@gmail.com>
2013-10-22 11:56:54 +0200

    Modifs compil

--- L-System/grammaire.c
+++ L-System/grammaire.c
@@ -20,25 +20,25 @@ void grammaire_lire(char *nomfic, Grammaire *g)
   fscanf(pfo, "%s\n", g->nom);
   
   /* Lecture du nombre de directions */
-  fscanf(pfo, "%d\n", &(g.turtle_dir_max));
+  fscanf(pfo, "%d\n", &(g->turtle_dir_max));
 
   /* Lecture de la diretion initiale */
-  fscanf(pfo, "%d\n", &(g.turtle_dir_init));
+  fscanf(pfo, "%d\n", &(g->turtle_dir_init));
 
   /* Lecture de l'axiome */
-  fscanf(pfo, "%s\n", g.axiome);
+  fscanf(pfo, "%s\n", g->axiome);
 
   /* Aucune règle au départ */
-  g.num=0 ;
+  g->num=0 ;
 
   /* Lecture des règles de production */
-  while (fscanf(pfo, "%c.%s\n", &g.car[g.num], g.regle[g.num]) != EOF)
+  while (fscanf(pfo, "%c.%s\n", &g->car[g->num], g->regle[g->num]) != EOF)
     {
       /* Calcul de la longueur de la partie droite de la règle */
-      g.long_regle[g.num] = strlen(g.regle[g.num]);
+      g->long_regle[g->num] = strlen(g->regle[g->num]);
       
       /* Il y a maintenant une règle de plus... */
-      g.num++;
+      g->num++;
 
       /* Le fichier est traité, on le ferme */
       fclose(pfo);
--- L-System/main.c
+++ L-System/main.c
@@ -1,8 +1,10 @@
+#include "grammaire.h"
+
 int main(int ac, char **av)
 {
   Grammaire *g;
 
-  grammaire_lire(av[1], g); 
+  grammaire_lire(av[1], *g); 
   grammaire_lire(g);
 
   return (0);
