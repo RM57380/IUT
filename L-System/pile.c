c6e1bc55206663a87aea7f52ee1257c1836fe452
Romaric Mangin <romaric.mangin0947@gmail.com>
2013-10-25 15:06:11 +0200

    Debut liste

---
+++ L-System/pile.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include "pile.h"
+
+Pile pile_initialiser(Pile_Cellule *pile)
+{
+  pile->x = 0;
+  pile->y = 0;
+  pile->dir = 0;
+
+  return (pile);
+}
+
+int pile_vide(Pile p)
+{
+  if (p == NULL)
+    return (1);
+  return (0);
+}
