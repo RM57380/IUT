ffb8f3732f25cf986197f01a69a3bf0bbb8e9a31
Romaric Mangin <romaric.mangin0947@gmail.com>
2013-10-31 15:44:34 +0100

    tools.h

---
+++ L-System/tools.h
@@ -0,0 +1,12 @@
+#ifndef _TOOLS_H_
+#define _TOOLS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Alloue dynamiquement la place pour une chaine de caracteres */
+char *tools_nouvelle_chaine(int length);
+
+#endif
